5ee1401a27999d0d0d123f28233c8910f693c251
Jeremy Rucker <JeremyR327@gmail.com>
2022-02-03 19:21:35 -0500

    First commit after porting jsfiddle to Atom

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+$('#monday').on('click', function ()
+{
+$('.right-half').html(" Monday <img src='https://upload.wikimedia.org/wikipedia/en/thumb/4/42/Beatles_-_Abbey_Road.jpg/220px-Beatles_-_Abbey_Road.jpg'> The Beatles")
+})
+
+$('#tuesday').on('click', function ()
+{
+$('.right-half').html("Tuesday <img src='https://upload.wikimedia.org/wikipedia/en/d/d4/Incredible_Jazz_Guitar.jpg'> Wes Montgomery")
+})
+
+$('#wednesday').on('click', function ()
+{
+$('.right-half').html("Wednesday <img src='https://upload.wikimedia.org/wikipedia/en/thumb/4/42/Beatles_-_Abbey_Road.jpg/220px-Beatles_-_Abbey_Road.jpg'> The Beatles")
+})
+
+$('#thursday').on('click', function ()
+{
+$('.right-half').html("Thursday <img src='https://upload.wikimedia.org/wikipedia/en/f/f6/Kendrick_Lamar_-_To_Pimp_a_Butterfly.png'> Kendrick Lamar")
+})
+
+$('#friday').on('click', function ()
+{
+$('.right-half').html("Friday <img src='https://upload.wikimedia.org/wikipedia/en/6/6e/Oscarnighttrain.jpg'> Oscar Peterson Trio")
+})
+
+$('#saturday').on('click', function ()
+{
+$('.right-half').html("Saturday <img src='https://upload.wikimedia.org/wikipedia/en/thumb/e/e6/Stadiumarcadium.jpg/220px-Stadiumarcadium.jpg'> Red Hot Chili Peppers")
+})
+
+$('#sunday').on('click', function ()
+{
+$('.right-half').html("Sunday <img src='https://img.discogs.com/N8gesMGLJRkgWb8231aRMw6wxJ4=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-4625247-1370296315-6051.jpeg.jpg'> Django Reinhardt")
+})
