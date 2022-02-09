6922e11a3ace4529d8e9e79c948562d682b54ad4
Jeremy Rucker <JeremyR327@gmail.com>
2022-02-09 11:24:20 -0500

    added hyperlinks to artists and title button

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,16 @@
 // Consolidate the above listeners into one listener
-  $('.day-container').on('click', function () {
+$('.day-container').on('click', function () {
   var albumArt = $(this).data('album-art')
   var dayOfWeek = $(this).data('dow')
   var artistName = $(this).data('artist-name')
   console.log(albumArt)
   console.log(dayOfWeek)
   console.log(artistName)
-
+  $(".right-half").show()
   $(".right-half").html(`${dayOfWeek}<img src='${albumArt}'/>${artistName}`)
+})
+
+$('.title').on('click', function () {
+  $(".right-half").hide()
+  console.log('hidden')
 })
